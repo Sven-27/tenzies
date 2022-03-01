4239b24a64b791d56895d4f5c323ee5450e5abfb
Sven-27 <sven.notermans@live.nl>
2022-03-01 20:06:44 +0100

    added title and description of game

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="app">
-      start of tenzies
-    </div>
+    <main className="app">
+      <div className="app-container">
+        <h2>Tenzies</h2>
+        <p>Roll until all dice are the same.
+           Click each die to freeze it at its 
+           current value between rolls.      
+        </p>
+      </div>
+    </main>
   );
 }
 
